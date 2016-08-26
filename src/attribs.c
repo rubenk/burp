fd841e016466b2f9a31de513f29ac1d035e0c2c0
Ruben Kerkhof <ruben@rubenkerkhof.com>
2016-08-26 17:58:36 +0200

    attribs.c: remove unused function

--- src/attribs.c
+++ src/attribs.c
@@ -275,15 +275,6 @@ uint64_t decode_file_no(struct iobuf *iobuf)
 	return (uint64_t)val;
 }
 
-uint64_t decode_file_no_and_save_path(struct iobuf *iobuf, char **save_path)
-{
-	int64_t val;
-	char *p=iobuf->buf;
-	p+=from_base64(&val, iobuf->buf);
-	*save_path=p+1;
-	return (uint64_t)val;
-}
-
 #ifdef HAVE_LUTIMES
 static int do_lutimes(const char *path, struct stat *statp)
 {
--- src/attribs.h
+++ src/attribs.h
@@ -11,7 +11,5 @@ extern int attribs_set(struct asfd *asfd, const char *path, struct stat *statp,
 	uint64_t winattr, struct cntr *cntr);
 
 extern uint64_t decode_file_no(struct iobuf *iobuf);
-extern uint64_t decode_file_no_and_save_path(struct iobuf *iobuf,
-	char **save_path);
 
 #endif
